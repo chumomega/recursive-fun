4163b99c68802d99a3eb826c19448132e54c6c72
Chuma Okoro <cokoro@etsy.com>
2021-05-03 16:39:15 -0400

    add comments to scala file

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -13,16 +13,19 @@ object RecFun extends RecFunInterface {
 
   /**
    * Exercise 1
+   * get the nnumber in pascals triangle given a column and row number
    */
   def pascal(c: Int, r: Int): Int = ???
 
   /**
    * Exercise 2
+   * this will take in a list of char and we need to say if its a balanced array of parentheses or not
    */
   def balance(chars: List[Char]): Boolean = ???
 
   /**
    * Exercise 3
+   * how many ways are there to make change given the denominations of coins
    */
   def countChange(money: Int, coins: List[Int]): Int = ???
 }
