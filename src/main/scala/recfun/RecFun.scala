1263840d58b4f0fa9b1c9b58f69778f176ae24d1
Chuma Okoro <cokoro@etsy.com>
2021-05-04 09:43:17 -0400

    add code for balancing the parenthesis

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -1,5 +1,8 @@
 package recfun
 
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 object RecFun extends RecFunInterface {
 
   def main(args: Array[String]): Unit = {
@@ -21,8 +24,32 @@ object RecFun extends RecFunInterface {
    * Exercise 2
    * this will take in a list of char and we need to say if its a balanced array of parentheses or not
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def stackRec(paren: mutable.Stack[Char], chars: List[Char]): Boolean = {
+      if (chars.isEmpty) true
+      else {
 
+        if (chars.head == ')' && paren.isEmpty) {
+          false
+        }
+        else {
+          if (chars.head == ')' && paren.top == '(') {
+            paren.pop()
+          }
+
+          if (chars.head == '(') {
+            paren.push(chars.head)
+          }
+          stackRec(paren, chars.tail)
+        }
+      }
+
+    }
+    stackRec(mutable.Stack[Char](), chars)
+  }
+
+  balance("hllo(world)xyz".toList)
   /**
    * Exercise 3
    * how many ways are there to make change given the denominations of coins
